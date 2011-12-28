569a0789bc7706def9e51fcf961986a1fd57900f
Kight <w.jq0722@gmail.com>
2011-12-27 21:42:38 -0800

    minor

--- kern/monitor.c
+++ kern/monitor.c
@@ -23,6 +23,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display back trace information of function", mon_backtrace },
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
